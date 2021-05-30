b05a70e0098cd2ff7a091590a4c1c0e489c8ce52
kimnanhee <kimnanhee0225@gmail.com>
2021-05-30 23:25:10 +0900

    add : add lde_matrix.ino

--- festival-led/led_matrix/led_matrix.ino
+++ festival-led/led_matrix/led_matrix.ino
@@ -1,18 +1,19 @@
 #include <LedControl.h>
-#define MAX 16
+#define MAX 8
 LedControl lc = LedControl(12, 11, 10, MAX);
 
 String str;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   for (int num = 0; num < MAX; num++)
   {
     lc.shutdown(num, false); // 절전모드 off
     lc.setIntensity(num, 2); // LED brightness set(0~15)
     lc.clearDisplay(num); // LED clear
   }
+  pinMode(13, OUTPUT);
 }
 
 void loop()
@@ -21,19 +22,25 @@ void loop()
   {
     char data = Serial.read();
     Serial.println(data);
-    if(data == '2') str = ""; // start byte
-    else if(data == '3') Serial.println(str); // end byte
+    if(data == 0x02) str = ""; // start byte
+    else if(data == 0x03) 
+    {
+      if(str == "1111") digitalWrite(13, HIGH);
+      else if(str == "0000") digitalWrite(13, LOW);
+      
+      lc.setLed(atoi(str[0]), atoi(str[1]), atoi(str[2]), atoi(str[3]));
+    }
     else str += data;
   }
-  /*
+  
   // 도트매트릭스의 LED를 첫번째 부터 1개씩 차례대로 ON
   for (int num = 0; num < MAX; num++)
   {
-    for (int row = 7; row >= 0; row--)
+    for (int row = 0; row < 8; row++)
     {
       for (int col = 0; col < 8; col++)
       {
-        lc.setLed(num, col, row, true);
+        lc.setLed(num, row, col, true);
         delay(25);
       }
     }
@@ -42,14 +49,14 @@ void loop()
   // 도트매트릭스의 LED를 첫번째 부터 1개씩 차례대로 OFF
   for (int num = 0; num < MAX; num++)
   {
-    for (int row = 7; row >= 0; row--)
+    for (int row = 0; row < 8; row++)
     {
       for (int col = 0; col < 8; col++)
       {
-        lc.setLed(num, col, row, false);
+        lc.setLed(num, row, col, false);
         delay(25);
       }
     }
   }
-  */
+  
 }
