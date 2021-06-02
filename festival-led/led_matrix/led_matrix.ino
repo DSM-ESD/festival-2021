5d54596bcce4f5fc83ba5fd0bb0d90121ce626bd
kimnanhee <kimnanhee0225@gmail.com>
2021-06-02 21:51:06 +0900

    add : add led matrix control with serial protocol

--- festival-led/led_matrix/led_matrix.ino
+++ festival-led/led_matrix/led_matrix.ino
@@ -2,37 +2,18 @@
 #define MAX 8
 LedControl lc = LedControl(12, 11, 10, MAX);
 
-String str;
-
 void setup()
 {
-  Serial.begin(115200);
   for (int num = 0; num < MAX; num++)
   {
     lc.shutdown(num, false); // 절전모드 off
-    lc.setIntensity(num, 2); // LED brightness set(0~15)
+    lc.setIntensity(num, 8); // LED brightness set(0~15)
     lc.clearDisplay(num); // LED clear
   }
-  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-  if(Serial.available())
-  {
-    char data = Serial.read();
-    Serial.println(data);
-    if(data == 0x02) str = ""; // start byte
-    else if(data == 0x03) 
-    {
-      if(str == "1111") digitalWrite(13, HIGH);
-      else if(str == "0000") digitalWrite(13, LOW);
-      
-      lc.setLed(atoi(str[0]), atoi(str[1]), atoi(str[2]), atoi(str[3]));
-    }
-    else str += data;
-  }
-  
   // 도트매트릭스의 LED를 첫번째 부터 1개씩 차례대로 ON
   for (int num = 0; num < MAX; num++)
   {
@@ -58,5 +39,4 @@ void loop()
       }
     }
   }
-  
 }
---
+++ festival-led/led_matrix_serial/led_matrix_serial.ino
@@ -0,0 +1,35 @@
+#include <LedControl.h>
+#define MAX 8
+LedControl lc = LedControl(12, 11, 10, MAX);
+
+String str;
+
+void setup()
+{
+  Serial.begin(115200);
+  for (int num = 0; num < MAX; num++)
+  {
+    lc.shutdown(num, false); // 절전모드 off
+    lc.setIntensity(num, 8); // LED brightness set(0~15)
+    lc.clearDisplay(num); // LED clear
+  }
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    char data = Serial.read();
+    Serial.println(data);
+
+    // led matrix control protocol
+    // 0x02 num row col 0x03
+
+    if (data == 0x02) str = "";
+    else if (data == 0x03)
+    {
+      lc.setLed(atoi(str[0]), atoi(str[1]), atoi(str[2]), true);
+    }
+    else str += data;
+  }
+}
