165022fa1590cc04655996606309720905660ebd
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-06-02 19:54:33 +0900

    Update test.py
    파일업로드 완료 및 오류 해결

--- handline_AI_test/test.py
+++ handline_AI_test/test.py
@@ -1,15 +1,8 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://handson.ga/")
-a = driver.find_element_by_class_name("file-upload-input")
-#driver.find_element_by_css_selector("input[type='file']").send_keys(r"/Users/kimminyoung/Desktop/workspace/crawler/test.jpeg")
-#test = driver.find_element_by_css_selector('input[type="file"]')
-print('1')
-print(a)
-a = a.send_keys(r"/Users/kimminyoung/Desktop/workspace/crawler/test.jpeg")
-#test.send_keys(r'/Users/kimminyoung/Desktop/workspace/crawler/test.jpeg')
-print(a)
-print('2s')
-driver.close()
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(executable_path = 'C:/Users/DSM/Desktop/study/chromedriver_win32/chromedriver.exe')
+driver.get("https://handson.ga/index.html")
+search = driver.find_element_by_css_selector("input[type='file']").send_keys(r"C:/Users/DSM/Desktop/study/hand.jpg")
+
