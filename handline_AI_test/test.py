88968e861a2c8c41a823c3146b6254fdbf943af4
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-06-02 22:41:21 +0900

    Update test.py
    검사결과가져오기

--- handline_AI_test/test.py
+++ handline_AI_test/test.py
@@ -5,4 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(executable_path = 'C:/Users/DSM/Desktop/study/chromedriver_win32/chromedriver.exe')
 driver.get("https://handson.ga/index.html")
 search = driver.find_element_by_css_selector("input[type='file']").send_keys(r"C:/Users/DSM/Desktop/study/hand.jpg")
+time.sleep(2)
+a = driver.find_elements_by_class_name("son-label d-flex align-items-center")
 
+result = driver.find_elements_by_class_name("d-block percent-text")
+print(result)
