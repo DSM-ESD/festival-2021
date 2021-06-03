c63f7405c02b5a7708609c08b8be46ad1b8bfc3c
MoonSeoJun <msj00130@gmail.com>
2021-06-03 18:50:37 +0900

    add : add load the game data

--- festival-game/gms/gms-backend/GMS_Backend/app.py
+++ festival-game/gms/gms-backend/GMS_Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 from sqlalchemy import create_engine, text
 
 def create_app():
@@ -10,20 +10,42 @@ def create_app():
 
     @app.route('/save-game-data', methods = ['POST'])
     def save_game_data():
-        game_data = request.json
-        user_id = app.database.execute(text("""
-                                            INSERT INTO game_result (
-                                            id,
-                                            score,
-                                            game_type
-                                           ) VALUES (
-                                            :id,
-                                            :score,
-                                            game_type
-                                           )
-                                            """), game_data).lastrowid
+        try:
+            game_data = request.json
+            user_id = app.database.execute(text("""
+                                                INSERT INTO game_result (
+                                                id,
+                                                score,
+                                                game_type
+                                            ) VALUES (
+                                                :id,
+                                                :score,
+                                                :game_type
+                                            )
+                                                """), game_data).lastrowid
+            return jsonify(message="Create Success"), 201
+        except:
+            return jsonify(message="Create Fail"), 500
 
-        return "", 200
+    @app.route('/load-first-game/<game_type>', methods = ['GET'])
+    def load_first_game(game_type):
+        try:
+            recive_game_data = app.database.execute(text(f"SELECT * FROM game_result WHERE game_type = \"{game_type}\" ORDER BY score DESC"))
+            game_data_arr = recive_game_data.fetchall()
+
+            json_list = []
+            number = 1
+
+            for row in game_data_arr:
+                json_list.append({f"user{number}": {"id" : row[0], "score" : row[1], "game_type" : row[2]}})
+                number += 1
+
+            if json_list == []:
+                return jsonify(message="Game datas are empty"), 200
+
+            return jsonify(json_list), 200
+        except:
+            return jsonify(message="Load Fail"), 500
 
 
     @app.route("/")
