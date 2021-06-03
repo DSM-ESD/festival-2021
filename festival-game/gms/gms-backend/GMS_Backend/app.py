060e2ba284ca559dd6fa39feb1af253d4c1dcacc
MoonSeoJun <msj00130@gmail.com>
2021-06-03 16:33:45 +0900

    add : add save the game datas api

--- festival-game/gms/gms-backend/GMS_Backend/app.py
+++ festival-game/gms/gms-backend/GMS_Backend/app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine, text
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8')
+    app.database = database
+
+    @app.route('/save-game-data', methods = ['POST'])
+    def save_game_data():
+        game_data = request.json
+        user_id = app.database.execute(text("""
+                                            INSERT INTO game_result (
+                                            id,
+                                            score,
+                                            game_type
+                                           ) VALUES (
+                                            :id,
+                                            :score,
+                                            game_type
+                                           )
+                                            """), game_data).lastrowid
+
+        return "", 200
+
+
+    @app.route("/")
+    def hello_world():
+        return "Hello World"
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
+
     app.run()
